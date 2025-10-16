6fb08418f2b320c1fb252e5848a5761359dcda72
maniebra <mani.ebra@gmail.com>
2025-10-16 18:40:38 +0330

    add crud base entity

--- ReSTerAvecMoi/Generics/CrudEntityBase.cs
+++ ReSTerAvecMoi/Generics/CrudEntityBase.cs
@@ -1,6 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ReSTerAvecMoi.Generics;
 
-public class CrudEntityBase
+public class CrudEntityBase<TKey>
+where TKey : IComparable<TKey>, IQueryable<TKey>
 {
+    [Key]
+    public virtual TKey Id { get; init; }
     
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public DateTimeOffset CreatedAt { get; init; }
+    
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public DateTimeOffset? UpdatedAt { get; set; }
 }
