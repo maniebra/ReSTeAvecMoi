7d2a7c70a44e0b03e7caa205a285fc7dc2a8ec4f
maniebra <mani.ebra@gmail.com>
2025-10-19 12:26:25 +0330

    add constructor for entity not found exception

--- ReSTeAvecMoi/Exceptions/EntityNotFoundException.cs
+++ ReSTeAvecMoi/Exceptions/EntityNotFoundException.cs
@@ -1,4 +1,10 @@
 namespace ReSTeAvecMoi.Exceptions;
 
 public class EntityNotFoundException<TEntity> : Exception
-where TEntity : class { }
+    where TEntity : class
+{
+
+    public EntityNotFoundException(string message) : base(message) { }
+
+    public EntityNotFoundException() : base($"No such {nameof(TEntity)}!") {}
+}
